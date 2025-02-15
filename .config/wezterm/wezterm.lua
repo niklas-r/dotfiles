a2e6d6c2542e4c251c867829d272a158c5fa0eff
Niklas Kahn <niklas@neatminds.se>
2025-02-15 21:10:41 +0100

    Add smart-split integration again

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+local smart_splits = wezterm.plugin.require 'https://github.com/mrjones2014/smart-splits.nvim'
 local act = wezterm.action
 local features = require 'features'
 local utils = require 'utils'
@@ -299,6 +300,18 @@ config.window_padding = {
 
 }
 --]]
+
+smart_splits.apply_to_config(config, {
+  -- directional keys to use in order of: left, down, up, right
+  direction_keys = { 'h', 'j', 'k', 'l' },
+  -- modifier keys to combine with direction_keys
+  modifiers = {
+    move = 'CTRL', -- modifier to use for pane movement, e.g. CTRL+h to move left
+    resize = 'META', -- modifier to use for pane resize, e.g. META+h to resize to the left
+  },
+  -- log level to use: info, warn, error
+  log_level = 'info',
+})
 
 -- and finally, return the configuration to wezterm
 return config
