35dcd661289ec0749c23bc2b74eb37b7bf84e1da
Niklas Kahn <niklas@neatminds.se>
2025-02-15 21:10:42 +0100

    New font! And going back to Tokyo

--- .config/wezterm/globals.lua
+++ .config/wezterm/globals.lua
@@ -1,11 +1,11 @@
 return {
   opacity = 1,
+  colorscheme = "Tokyo Night Moon",
   padding = {
+    bottom = 20,
     top = 20,
     right = 20,
-    bottom = 20,
     left = 20,
   },
-  font = "CaskaydiaMono Nerd Font",
-  colorscheme = "Ayu Mirage",
+  font = "Maple Mono NF",
 }
