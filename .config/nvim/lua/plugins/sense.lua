1ee79b0a6bb6c8185a4156d9d45a9de8a94ff0f2
Niklas Kahn <niklas@neatminds.se>
2025-03-13 16:56:57 +0100

    Add sense nvim plugin

---
+++ .config/nvim/lua/plugins/sense.lua
@@ -0,0 +1,24 @@
+-- This plugin shows diagnostics in the statuscolumn and virtual text outside of the window.
+return {
+  'boltlessengineer/sense.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  init = function()
+    -- Unorthodox way for configuring this Lazy plugin...
+    ---@type sense.Config
+    vim.g.sense_nvim = {
+      presets = {
+        ---@type sense.Config.Presets.VirtualText
+        virtualtext = {
+          enabled = true,
+        },
+        ---@type sense.Config.Presets.StatusColumn
+        statuscolumn = {
+          enabled = true,
+        },
+      },
+    }
+
+    -- Just require to get LSP to read types
+    require 'sense'
+  end,
+}
