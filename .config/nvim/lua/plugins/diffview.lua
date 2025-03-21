75d07ac5d39e9489896bbac55f80e4e5c871d5f2
Niklas Kahn <niklas@neatminds.se>
2025-03-21 09:02:57 +0100

    adding diffview
    it's actually pretty nice

---
+++ .config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'sindrets/diffview.nvim',
+    event = 'BufReadPre',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    },
+  },
+}
