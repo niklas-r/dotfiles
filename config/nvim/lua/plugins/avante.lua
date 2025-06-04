7eee8ae5f8aba203a2f4a81b655ea212c3aa0148
Niklas Kahn <niklas@neatminds.se>
2025-06-04 11:11:40 +0200

    Update nvim plugins and fix Avante config

--- config/nvim/lua/plugins/avante.lua
+++ config/nvim/lua/plugins/avante.lua
@@ -14,25 +14,32 @@ return {
       -- provider_opts = {},
     },
     ---@alias avante.Mode "agentic" | "legacy"
-    mode = 'legacy', -- agentic mode is still performing very badly
-    openai = {
-      endpoint = 'https://api.openai.com/v1',
-      model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-      temperature = 0,
-      max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-      disable_tools = true, -- I can't get tools working with GPT-4o, maybe not supported?
-    },
-    claude = {
-      endpoint = 'https://api.anthropic.com',
-      -- model = 'claude-3-7-sonnet-20250219',
-      -- model = 'claude-3-7-sonnet-latest', -- Not test yet with latest
-      model = 'claude-sonnet-4-20250514',
-      timeout = 30000, -- Timeout in milliseconds
-      temperature = 0,
-      max_tokens = 8000,
-      disable_tools = true, -- I can't get tools working with Claude, maybe not supported?
+    mode = 'agentic', -- legacy mode has been deprecated...
+    providers = {
+      openai = {
+        endpoint = 'https://api.openai.com/v1',
+        model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+        extra_request_body = {
+          temperature = 0,
+          max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+        },
+        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+        disable_tools = true, -- I can't get tools working with GPT-4o, maybe not supported?
+      },
+      claude = {
+        endpoint = 'https://api.anthropic.com',
+        model = 'claude-3-5-sonnet-20241022',
+        -- model = 'claude-3-7-sonnet-20250219',
+        -- model = 'claude-3-7-sonnet-latest',
+        -- model = 'claude-sonnet-4-20250514',
+        timeout = 30000, -- Timeout in milliseconds
+        extra_request_body = {
+          temperature = 0.15,
+          max_tokens = 4000,
+        },
+        disable_tools = true, -- Tools does not work with Claude Sonnet 4 yet
+      },
     },
     custom_tools = {
       {
