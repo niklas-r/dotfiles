4a5f66234a4ede03621e45263461b28718b6b9e2
Niklas Kahn <niklas@neatminds.se>
2025-06-17 17:15:01 +0200

    Add nvim-coverage plugin

---
+++ config/nvim/lua/plugins/coverage.lua
@@ -0,0 +1,51 @@
+vim.g.display_code_coverage = false
+
+return {
+  'andythigpen/nvim-coverage',
+  version = '*',
+  dependencies = {
+    'folke/which-key.nvim',
+    'folke/snacks.nvim',
+  },
+  cmd = { 'Coverage', 'CoverageLoad', 'CoverageLoadLcov', 'CoverageShow', 'CoverageHide', 'CoverageToggle', 'CoverageClear', 'CoverageSummary' },
+  init = function()
+    local wk = require 'which-key'
+    wk.add {
+      { '<leader>uc', group = '[C]ode coverage' },
+    }
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'VeryLazy',
+      callback = function()
+        Snacks.toggle({
+          name = 'code coverage',
+          get = function()
+            return vim.g.display_code_coverage
+          end,
+          set = function(state)
+            if state then
+              vim.cmd 'Coverage'
+            else
+              vim.cmd 'CoverageClear'
+            end
+
+            vim.g.display_code_coverage = state
+          end,
+        }):map '<leader>uct'
+      end,
+    })
+  end,
+  keys = {
+    {
+      '<leader>ucs',
+      '<CMD>CoverageSummary<CR>',
+      desc = '[S]ummary',
+    },
+  },
+  opts = {
+    auto_reload = true,
+    oad_coverage_cb = function(ftype)
+      vim.notify('Loaded ' .. ftype .. ' coverage')
+    end,
+  },
+}
--- config/nvim/lua/plugins/neotest.lua
+++ config/nvim/lua/plugins/neotest.lua
@@ -9,7 +9,7 @@ return {
     -- Vitest
     'marilari88/neotest-vitest',
   },
-  config = function(opts)
+  config = function()
     require('neotest').setup {
       adapters = {
         require 'neotest-vitest' {
@@ -28,63 +28,63 @@ return {
       function()
         require('neotest').summary.toggle()
       end,
-      desc = '[U]nit Test [S]ummary',
+      desc = '[S]ummary',
     },
     {
       '<leader>uo',
       function()
         require('neotest').output()
       end,
-      desc = '[U]nit Test [O]utput',
+      desc = '[O]utput',
     },
     {
       '<leader>up',
       function()
         require('neotest').output_panel()
       end,
-      desc = '[U]nit Test Output [P]anel',
+      desc = 'Output [P]anel',
     },
     {
       '<leader>urt',
       function()
         require('neotest').run.run()
       end,
-      desc = '[U]nit Test [R]un Nearest [T]est',
+      desc = '[R]un Nearest [T]est',
     },
     {
       '<leader>urf',
       function()
         require('neotest').run.run(vim.fn.expand '%')
       end,
-      desc = '[U]nit Test [R]un Current [F]ile',
+      desc = '[R]un Current [F]ile',
     },
     {
       '<leader>url',
       function()
         require('neotest').run.run_last()
       end,
-      desc = '[U]nit Test [R]un: [L]ast Test',
+      desc = '[R]un: [L]ast Test',
     },
     {
       '<leader>udt',
       function()
         require('neotest').run.run { strategy = 'dap' }
       end,
-      desc = '[U]nit Test [D]ebug: Nearest [T]est',
+      desc = '[D]ebug: Nearest [T]est',
     },
     {
       '<leader>udl',
       function()
         require('neotest').run.run_last { strategy = 'dap' }
       end,
-      desc = '[U]nit Test [D]ebug: [L]ast Test',
+      desc = '[D]ebug: [L]ast Test',
     },
     {
       '<leader>ua',
       function()
         require('neotest').run.attach()
       end,
-      desc = '[U]nit Test [A]ttach Nearest Test',
+      desc = '[A]ttach Nearest Test',
     },
   },
 }
