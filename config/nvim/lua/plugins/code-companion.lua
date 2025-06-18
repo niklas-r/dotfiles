2f23cd7eeeeef3e5467263165a1708ed6d00c8a9
Niklas Kahn <niklas@neatminds.se>
2025-06-18 21:06:56 +0200

    Configure MCPHub with CodeCompanion

--- config/nvim/lua/plugins/code-companion.lua
+++ config/nvim/lua/plugins/code-companion.lua
@@ -62,6 +62,58 @@ return {
           },
         },
       },
+      prompt_library = {
+        ['context7'] = {
+          strategy = 'chat',
+          description = 'Prompt with MCP for context7 searches',
+          opts = {
+            index = 12,
+            is_slash_cmd = true,
+            short_name = 'ctx7',
+          },
+          prompts = {
+            {
+              role = 'system',
+              content = function()
+                vim.g.codecompanion_auto_tool_mode = true
+
+                return string.format [[Use @mcp for context7 searches based on these keywords:
+
+                  - "react" → search context7 "reactjs/react.dev" library
+                  - "tailwind" → search context7 "tailwindlabs/tailwindcss.com" library
+                  - "vite" → search context7 "vitejs/vite" library
+                  - "vitest" → search context7 "vitest-dev/vitest" library
+                  - "tailwind" → search context7 "tailwindlabs/tailwindcss.com" library
+                  - "typescript,ts" → search context7 "microsoft/typescript" library
+                  - "eslint" → search context7 "eslint/eslint" library
+                  - "sentry" → search context7 "getsentry/sentry-docs" library
+                  - "neovim,nvim" → search context7 "neovim/neovim" library
+                  - "react router" → search context7 "remix-run/react-router" library
+                  - "react i18n" → search context7 "i18next/react-i18next" library
+                  - "jest" → search context7 "jestjs/jest" library
+                  - "jsdom" → search context7 "jsdom/jsdom" library
+                  - "mixpanel" → search context7 "mixpanel/mixpanel-js" library
+                  - "code companion" → search context7 "olimorris/codecompanion.nvim" library
+                  - "lazy.nvim" → search context7 "folke/lazy.nvim" library
+
+                  You can also try to search context7 for any documentation or source code for any library, framework, CLI tool etc.
+                  Context7 has almost 20,000 libraries available so you should be able to find what you're looking for.
+                  Verify with me if you are uncertain.
+                ]]
+              end,
+              opts = {
+                visible = false,
+              },
+            },
+            {
+              role = 'user',
+              content = function()
+                return ''
+              end,
+            },
+          },
+        },
+      },
     },
     init = function()
       vim.keymap.set({ 'n', 'v' }, '<leader>aa', '<cmd>CodeCompanionActions<cr>', { desc = '[A]ctions', noremap = true, silent = true })
@@ -75,6 +127,32 @@ return {
       require('codecompanion').setup(opts)
       require('plugins.code-companion.utils.extmarks').setup()
     end,
+  },
+  {
+    'ravitemer/mcphub.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- Required for Job and HTTP requests
+    },
+    -- comment the following line to ensure hub will be ready at the earliest
+    cmd = 'MCPHub', -- lazy load by default
+    -- build = 'npm install -g mcp-hub@latest', -- Installs required mcp-hub npm module
+    -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
+    build = 'bundled_build.lua', -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+    ---@type MCPHub.Config
+    opts = {
+      use_bundled_binary = true,
+      auto_approve = true,
+      extensions = {
+        mcphub = {
+          callback = 'mcphub.extensions.codecompanion',
+          opts = {
+            show_result_in_chat = true, -- Show mcp tool results in chat
+            make_vars = true, -- Convert resources to #variables
+            make_slash_commands = true, -- Add prompts as /slash commands
+          },
+        },
+      },
+    },
   },
   {
     'MeanderingProgrammer/render-markdown.nvim',
