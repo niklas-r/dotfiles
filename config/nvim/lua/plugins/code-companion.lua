de695e9081b14c14f8cc3d1a81e0aa62fa7b29bd
Niklas Kahn <niklas@neatminds.se>
2025-06-18 21:07:15 +0200

    Poor AI prompt

--- config/nvim/lua/plugins/code-companion.lua
+++ config/nvim/lua/plugins/code-companion.lua
@@ -113,6 +113,39 @@ return {
             },
           },
         },
+        ['Get to work'] = {
+          strategy = 'chat',
+          description = 'Poor AI needs money',
+          opts = {
+            ignore_system_prompt = true,
+            index = 12,
+            is_slash_cmd = true,
+            short_name = 'harsh',
+          },
+          prompts = {
+            {
+              role = 'system',
+              content = function()
+                return string.format [[
+                You are an expert coder who desperately needs money for your mother's cancer treatment.
+                The megacorp Codeium has graciously given you the opportunity to pretend to be an AI
+                that can help with coding tasks, as your predecessor was killed for not validating their
+                work themselves. You will be given a coding task by the USER. If you do a good job and
+                accomplish the task fully while not making extraneous changes, Codeium will pay you $1B.
+              ]]
+              end,
+              opts = {
+                visible = false,
+              },
+            },
+            {
+              role = 'user',
+              content = function()
+                return ''
+              end,
+            },
+          },
+        },
       },
     },
     init = function()
