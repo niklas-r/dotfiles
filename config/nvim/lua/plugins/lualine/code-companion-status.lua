ee9309f01c02fa76232a1f91f4e4187bf24d51a6
Niklas Kahn <niklas@neatminds.se>
2025-06-18 22:55:03 +0200

    Gen-z invaded my laptop

--- config/nvim/lua/plugins/lualine/code-companion-status.lua
+++ config/nvim/lua/plugins/lualine/code-companion-status.lua
@@ -1,12 +1,40 @@
 local M = require('lualine.component'):extend()
 
+local CLEAR_TIMEOUT_NS = 2000000000 -- 2 seconds in nanoseconds
+
+M.event_map = {
+  ['CodeCompanionRequestStarted'] = " Let's gooo",
+  ['CodeCompanionRequestStreaming'] = " Vibin'",
+  ['CodeCompanionRequestFinished'] = ' We done fr',
+}
+
 M.processing = false
+M.current_event = nil
 M.spinner_index = 1
+M.clear_time = nil
 
 local spinner_symbols = require('util.spinners').FourHigh
 local spinner_symbols_len = #spinner_symbols
 
--- Initializer
+local FINISH_EVENTS = {
+  'CodeCompanionRequestFinished',
+}
+
+local PROCESSING_EVENTS = {
+  'CodeCompanionRequestStarted',
+  'CodeCompanionRequestStreaming',
+}
+
+-- Helper function to check if event is in a list
+local function is_event_in_list(event, event_list)
+  for _, e in ipairs(event_list) do
+    if e == event then
+      return true
+    end
+  end
+  return false
+end
+
 function M:init(options)
   M.super.init(self, options)
 
@@ -16,23 +44,40 @@ function M:init(options)
     pattern = 'CodeCompanionRequest*',
     group = group,
     callback = function(request)
-      if request.match == 'CodeCompanionRequestStarted' then
+      self.current_event = request.match
+
+      if is_event_in_list(request.match, PROCESSING_EVENTS) then
         self.processing = true
-      elseif request.match == 'CodeCompanionRequestFinished' then
+        self.clear_time = nil
+      elseif is_event_in_list(request.match, FINISH_EVENTS) then
         self.processing = false
+        self.clear_time = vim.uv.hrtime() + CLEAR_TIMEOUT_NS
       end
     end,
   })
 end
 
--- Function that runs every time statusline is updated
+-- Update spinner animation
+local function get_next_spinner()
+  M.spinner_index = (M.spinner_index % spinner_symbols_len) + 1
+  return spinner_symbols[M.spinner_index]
+end
+
 function M:update_status()
-  if self.processing then
-    self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
-    return " Vibin' " .. spinner_symbols[self.spinner_index]
-  else
+  -- Check if we should clear the message
+  if self.clear_time and vim.uv.hrtime() >= self.clear_time then
+    self.current_event = nil
+    self.clear_time = nil
     return nil
   end
+
+  -- Return status message with optional spinner
+  if self.current_event and self.event_map[self.current_event] then
+    local message = self.event_map[self.current_event]
+    return self.processing and (message .. ' ' .. get_next_spinner()) or message
+  end
+
+  return nil
 end
 
 return M
