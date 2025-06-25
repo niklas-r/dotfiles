1b54bba791468880ffe70aba5102325eaebd610d
Niklas Kahn <niklas@neatminds.se>
2025-06-25 13:02:11 +0200

    Fix MCPHub config

--- config/nvim/lua/plugins/code-companion.lua
+++ config/nvim/lua/plugins/code-companion.lua
@@ -250,16 +250,6 @@ return {
     opts = {
       use_bundled_binary = true,
       auto_approve = true,
-      extensions = {
-        mcphub = {
-          callback = 'mcphub.extensions.codecompanion',
-          opts = {
-            show_result_in_chat = true, -- Show mcp tool results in chat
-            make_vars = true, -- Convert resources to #variables
-            make_slash_commands = true, -- Add prompts as /slash commands
-          },
-        },
-      },
     },
   },
   {
