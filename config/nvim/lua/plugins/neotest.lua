06ce077d94b0a236d4201220cb84029dded63370
Niklas Kahn <niklas@neatminds.se>
2025-10-30 20:50:17 +0100

    Run tests with code coverage in neotest

--- config/nvim/lua/plugins/neotest.lua
+++ config/nvim/lua/plugins/neotest.lua
@@ -13,7 +13,7 @@ return {
     require('neotest').setup {
       adapters = {
         require 'neotest-vitest' {
-          vitestCommand = 'npx vitest run --coverage.enabled=false --reporter=dot',
+          vitestCommand = 'npx vitest run --coverage.enabled=true --coverage.reporter=lcov --reporter=dot',
         },
       },
       output = {
