7e81b1a7a6909051f8be979cf22ca85369db015d
Niklas Kahn <niklas@neatminds.se>
2025-11-11 21:58:22 +0100

    Some color tweaking

--- config/nvim/lua/plugins/incline.lua
+++ config/nvim/lua/plugins/incline.lua
@@ -1,5 +1,34 @@
-local SEP = ' '
+local symbols = {
+  readonly = '',
+  ellipsis = '…',
+}
+
+local sep = {
+  spacer = ' ',
+}
+
 local DIR_MAX_LEN = 3
+
+local highlights = {
+  filename = 'Bold',
+  modified = 'MatchParen',
+  newfile = 'Special',
+}
+
+local function is_readonly(props)
+  return vim.bo[props.buf].modifiable == false or vim.bo[props.buf].readonly == true
+end
+
+local function is_modified(props)
+  return vim.bo[props.buf].modified == true
+end
+
+local function is_new(props)
+  return not is_readonly(props)
+    and vim.bo[props.buf].buftype == ''
+    and not vim.wo[props.win].diff
+    and vim.fn.filereadable(vim.api.nvim_buf_get_name(props.buf)) == 0
+end
 
 local function split_path(path)
   local parts = vim.split(path, '/', { trimempty = true })
@@ -15,7 +44,6 @@ end
 local function shorten_dir(path)
   local parts = split_path(path)
   local slice = {} ---@type string[]
-  local ellipsis = '...'
   if #parts <= DIR_MAX_LEN then
     return parts
   else
@@ -24,7 +52,7 @@ local function shorten_dir(path)
     for i = 1, prefix_count do
       slice[i] = parts[i]
     end
-    slice[prefix_count + 1] = ellipsis
+    slice[prefix_count + 1] = symbols.ellipsis
     for i = 1, suffix_count do
       slice[prefix_count + 1 + i] = parts[#parts - suffix_count + i]
     end
@@ -38,7 +66,17 @@ local function render_icons(props)
   local helpers = require 'incline.helpers'
   local ft_icon, ft_color = devicons.get_icon_color(filename)
 
-  return ft_icon and { SEP, ft_icon, SEP, guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or ''
+  local result = {}
+
+  if ft_icon then
+    table.insert(result, { sep.spacer, ft_icon, sep.spacer, guibg = ft_color, guifg = helpers.contrast_color(ft_color) })
+  end
+
+  if is_readonly(props) then
+    table.insert(result, { sep.spacer, symbols.readonly, group = 'NonText' })
+  end
+
+  return result
 end
 
 local function render_dir(props)
@@ -57,8 +95,18 @@ local function render_filename(props)
   if filename == '' then
     filename = '[No Name]'
   end
-  local modified = vim.bo[props.buf].modified
-  return { filename, gui = modified and 'bold,italic' or 'bold' }
+
+  local name_hl = ''
+
+  if is_modified(props) then
+    name_hl = highlights.modified
+  elseif is_new(props) then
+    name_hl = highlights.newfile
+  else
+    name_hl = highlights.filename
+  end
+
+  return { filename, group = name_hl }
 end
 
 local function render_diag(props)
@@ -71,7 +119,7 @@ local function render_diag(props)
     })
     if count > 0 then
       table.insert(entries, {
-        SEP .. icon .. SEP .. count,
+        sep.spacer .. icon .. sep.spacer .. count,
         group = 'DiagnosticSign' .. severity,
       })
     end
@@ -89,7 +137,7 @@ local function render(props)
 
   if #diag > 0 then
     table.insert(results, {
-      { diag, SEP },
+      { diag, sep.spacer },
       guibg = 'none',
     })
   end
@@ -98,11 +146,8 @@ local function render(props)
     table.insert(results, icons)
   end
 
-  if #dir > 0 then
-    table.insert(results, { SEP, dir })
-  end
-  if #filename > 0 then
-    table.insert(results, { filename, SEP })
+  if #dir > 0 or #filename > 0 then
+    table.insert(results, { sep.spacer, dir or '', filename or '', sep.spacer })
   end
 
   return results
@@ -131,6 +176,12 @@ return {
       },
       ignore = {
         filetypes = { 'oil' },
+      },
+      highlight = {
+        groups = {
+          InclineNormal = { guibg = 'none' },
+          InclineNormalNC = { guibg = 'none' },
+        },
       },
       render = function(props)
         return {
