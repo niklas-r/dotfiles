4933488d258e6799bb29f5e437f8e89fa241d43d
Niklas Kahn <niklas@neatminds.se>
2026-03-27 11:28:13 +0100

    Remove TOML lua util in wezterm

--- config/wezterm/utils/globals.lua
+++ config/wezterm/utils/globals.lua
@@ -1,23 +1,20 @@
-local TOML = require 'utils.toml'
+local wezterm = require 'wezterm' ---@type Wezterm
 
 local M = {}
 
-M.globals_path = os.getenv 'HOME' .. '/.config/wezterm/globals.toml'
+M.wezterm_path = os.getenv 'HOME' .. '/.config/wezterm'
+M.globals_path = M.wezterm_path .. '/globals.toml'
 M.nvim_path = os.getenv 'HOME' .. '/.local/share/nvim'
 
 M.read_globals = function()
   local globals = M.read_from_file(M.globals_path)
-  -- NOTE: When wezterm.serde.toml_decode is available in stable builds, use it instead
-  -- return wezterm.serde.toml_decode(globals)
-  return TOML.parse(globals)
+  return wezterm.serde.toml_decode(globals)
 end
 
 M.set_globals = function(callback)
   local globals = M.read_globals()
   callback(globals)
-  -- NOTE: When wezterm.serde.toml_encode is available in stable builds, use it instead
-  -- M.writeToFile(M.globalsPath, wezterm.serde.toml_encode(globals))
-  M.write_to_file(M.globals_path, TOML.encode(globals))
+  M.write_to_file(M.globals_path, wezterm.serde.toml_encode(globals))
 end
 
 M.read_from_file = function(path)
